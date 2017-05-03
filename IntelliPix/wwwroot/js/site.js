7283f80c03c5a104c344a1575190d3aa895bd7ba
kidchenko <kidchenko@gmail.com>
2017-05-03 16:58:24 -0300

    Add lightbox and update to bootstrap 4-alpha6 and jQuery 3.1.1

--- IntelliPix/wwwroot/js/site.js
+++ IntelliPix/wwwroot/js/site.js
@@ -1,13 +1,18 @@
 ﻿(function ($) {
-    $(document).on('change', ':file', function () {
-        var input = $(this),
-        numFiles = input.get(0).files ? input.get(0).files.length : 1,
-        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-        input.trigger('fileselect', [numFiles, label])
-    });
+    $(document).ready(function () {
+        $(document).on('change', ':file', function () {
+            var input = $(this),
+                numFiles = input.get(0).files ? input.get(0).files.length : 1,
+                label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+            input.trigger('fileselect', [numFiles, label])
+        });
+
+        $(document).on('click', '[data-toggle="lightbox"]', function (event) {
+            event.preventDefault();
+            $(this).ekkoLightbox();
+        });
 
 
-    $(document).ready(function() {
         $(':file').on('fileselect', function(event, numFiles, label) {
             var input = $(this).parents('.input-group').find(':text');
             var form = $(this).parents('form');
